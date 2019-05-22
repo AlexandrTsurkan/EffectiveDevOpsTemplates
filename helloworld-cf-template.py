e5382620bbb395ae22e3a1f08339fbe5542d2057
AlexandrTsurkan <suzuki.dops@gmail.com>
2019-05-22 15:10:40 +0300

    Adding helloworld Troposphere template

--- helloworld-cf-template.py
+++ helloworld-cf-template.py
@@ -1,4 +1,6 @@
-"""Generating CloudFormation template.""" from troposphere import (
+"""Generating CloudFormation template."""
+
+from troposphere import (
     Base64,
     ec2,
     GetAtt,
@@ -6,11 +8,23 @@
     Output,
     Parameter,
     Ref,
-    Template, ) ApplicationPort = "3000" t = Template() t.add_description("Effective DevOps in AWS: HelloWorld web application") t.add_parameter(Parameter(
+    Template,
+)
+
+ApplicationPort = "3000"
+
+t = Template()
+
+t.add_description("Effective DevOps in AWS: HelloWorld web application")
+
+t.add_parameter(Parameter(
     "KeyPair",
     Description="Name of an existing EC2 KeyPair to SSH",
     Type="AWS::EC2::KeyPair::KeyName",
-    ConstraintDescription="must be the name of an existing EC2 KeyPair.", )) t.add_resource(ec2.SecurityGroup(
+    ConstraintDescription="must be the name of an existing EC2 KeyPair.",
+))
+
+t.add_resource(ec2.SecurityGroup(
     "SecurityGroup",
     GroupDescription="Allow SSH and TCP/{} access".format(ApplicationPort),
     SecurityGroupIngress=[
@@ -26,24 +40,39 @@
             ToPort=ApplicationPort,
             CidrIp="0.0.0.0/0",
         ),
-    ], )) ud = Base64(Join('\n', [
+    ],
+))
+
+ud = Base64(Join('\n', [
     "#!/bin/bash",
     "sudo yum install --enablerepo=epel -y nodejs",
     "wget http://bit.ly/2vESNuc -O /home/ec2-user/helloworld.js",
     "wget http://bit.ly/2vVvT18 -O /etc/init/helloworld.conf",
-    "start helloworld" ])) t.add_resource(ec2.Instance(
+    "start helloworld"
+]))
+
+t.add_resource(ec2.Instance(
     "instance",
-    ImageId="ami-a4c7edb2",
+    ImageId="ami-03a71cec707bfc3d7",
     InstanceType="t2.micro",
     SecurityGroups=[Ref("SecurityGroup")],
     KeyName=Ref("KeyPair"),
-    UserData=ud, )) t.add_output(Output(
+    UserData=ud,
+))
+
+t.add_output(Output(
     "InstancePublicIp",
     Description="Public IP of our instance.",
-    Value=GetAtt("instance", "PublicIp"), )) t.add_output(Output(
+    Value=GetAtt("instance", "PublicIp"),
+))
+
+t.add_output(Output(
     "WebUrl",
     Description="Application endpoint",
     Value=Join("", [
         "http://", GetAtt("instance", "PublicDnsName"),
         ":", ApplicationPort
-    ]), )) print t.to_json()
+    ]),
+))
+
+print t.to_json()
